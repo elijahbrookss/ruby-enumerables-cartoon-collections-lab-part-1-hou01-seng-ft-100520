8ad53cdc9328fc2665902322ab12822784db9fdb
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 13:57:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,7 @@ end
 
 def list_dwarves(array)
   array.each do |element|
-    count = 1
-    while count < array.length do
-      p count
-      count += 1
-    end
-    p count.to_s + element
-
+    
   end
 end
 
