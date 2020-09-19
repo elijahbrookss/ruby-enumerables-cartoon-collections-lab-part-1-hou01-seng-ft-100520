8f5583d0b04bb094bdc2c84b3d22820a08693eb0
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 13:55:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,13 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each{|element| p element + " "}
+  array.each do |element|
+    count = 1
+    while count < array.length do
+      p count.to_s + element
+      count += 1
+    end
+  end
 end
 
 list_dwarves(["Gimli", "Thorin", "Balin", "Gloin"])
